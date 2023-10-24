be2ced05b69798934d4ba82533fd772c55488ff3
belleb23 <bellebernieri@gmail.com>
2023-10-24 19:20:54 -0300

    register ui 1

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { Button } from "antd";
 
 function App() {
   return (
-    <div className="App p-5">
-      <h1>Adote Um Vira Lata</h1>
-      <Button>Primary Button</Button>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Form, Button, Input } from "antd";
+
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="authentication">
+      <div className="authentication-form card p-3">
+         <h1 className="card-title">Nice To Meet U</h1>
+         <Form layout="vertical">
+            <Form.Item label="Name" name="name"
+              // onFinish={onFinish}
+            >
+              <Input placeholder="Name" />
+            </Form.Item>
+            <Form.Item label="Email" name="email">
+              <Input placeholder="Email" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input placeholder="Password" type="password" />
+            </Form.Item>
+
+            <Button
+              className="primary-button my-2 full-width-button"
+              htmlType="submit"
+            >
+              REGISTER
+            </Button>
+        </Form>
+      </div>
+    </div>
   )
 }
 
