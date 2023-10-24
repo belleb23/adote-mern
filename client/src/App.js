1c9e2e78312d6d48764169cb4be87de352991db0
belleb23 <bellebernieri@gmail.com>
2023-10-24 19:09:29 -0300

    antd setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-5">
       <h1>Adote Um Vira Lata</h1>
+      <Button>Primary Button</Button>
     </div>
   );
 }
