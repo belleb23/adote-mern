7f3a94d160f885194250064c953fd2fd39124242
belleb23 <bellebernieri@gmail.com>
2023-10-25 14:41:57 -0300

    dynamic menu

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,104 +1,146 @@
-import React, { useState } from 'react'
-import '../layout.css';
+import React, { useState } from "react";
+import "../layout.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Badge } from "antd";
 
 function Layout({ children }) {
-    const [collapsed, setCollapsed] = useState(false);
-    const { user } = useSelector((state) => state.user);
-    const location = useLocation();
-    const navigate = useNavigate();
+  const [collapsed, setCollapsed] = useState(false);
+  const { user } = useSelector((state) => state.user);
 
-    const userMenu = [
-        {
-            name: "Home",
-            path: "/",
-            icon: "ri-home-line",
-        },
-        {
-            name: "Adoçoes",
-            path: "/applications",
-            icon: "ri-file-list-line",
-        },
-        {
-            name: "Visitas",
-            path: "/visits",
-            icon: "ri-hospital-line",
-        }
-    ];
-        
-    const menuToBeRendered =  userMenu;
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    return (
+  const userMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-line",
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+      icon: "ri-file-list-line",
+    },
+    {
+      name: "Application",
+      path: "/applications",
+      icon: "ri-hospital-line",
+    }
+  ];
+
+  const volunterMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-line",
+    },
+    {
+      name: "Appointments",
+      path: "/volunter/appointments",
+      icon: "ri-file-list-line",
+    },
+    {
+      name: "Profile",
+      path: `/volunter/profile/${user?._id}`,
+      icon: "ri-user-line",
+    },
+  ];
+
+  const adminMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-line",
+    },
+    {
+      name: "Users",
+      path: "/admin/userslist",
+      icon: "ri-user-line",
+    },
+    {
+      name: "Volunter",
+      path: "/admin/volunteerslist",
+      icon: "ri-user-star-line",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "ri-user-line",
+    },
+  ];
+
+  const menuToBeRendered = user?.isAdmin ? adminMenu : user?.isVolunter ? volunterMenu : userMenu;
+  const role = user?.isAdmin ? "Admin" : user?.isVolunter ? "Volunter" : "User";
+  return (
     <div className="main">
-        <div className="d-flex layout">
+      <div className="d-flex layout">
         <div className="sidebar">
-            <div className="sidebar-header">
-            <h1 className="logo">Adote VL</h1>
-            
-            </div>
+          <div className="sidebar-header">
+            <h1 className="logo">SH</h1>
+            <h1 className="role">{role}</h1>
+          </div>
 
-            <div className="menu">
+          <div className="menu">
             {menuToBeRendered.map((menu) => {
-                const isActive = location.pathname === menu.path;
-                return (
+              const isActive = location.pathname === menu.path;
+              return (
                 <div
-                    className={`d-flex menu-item ${
+                  className={`d-flex menu-item ${
                     isActive && "active-menu-item"
-                    }`}
+                  }`}
                 >
-                    <i className={menu.icon}></i>
-                    {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
+                  <i className={menu.icon}></i>
+                  {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
                 </div>
-                );
+              );
             })}
             <div
-                className={`d-flex menu-item `}
-                onClick={() => {
+              className={`d-flex menu-item `}
+              onClick={() => {
                 localStorage.clear();
                 navigate("/login");
-                }}
+              }}
             >
-                <i className="ri-logout-circle-line"></i>
-                {!collapsed && <Link to="/login">Logout</Link>}
+              <i className="ri-logout-circle-line"></i>
+              {!collapsed && <Link to="/login">Logout</Link>}
             </div>
-            </div>
+          </div>
         </div>
 
         <div className="content">
-            <div className="header">
+          <div className="header">
             {collapsed ? (
-                <i
+              <i
                 className="ri-menu-2-fill header-action-icon"
                 onClick={() => setCollapsed(false)}
-                ></i>
+              ></i>
             ) : (
-                <i
+              <i
                 className="ri-close-fill header-action-icon"
                 onClick={() => setCollapsed(true)}
-                ></i>
+              ></i>
             )}
 
             <div className="d-flex align-items-center px-4">
-                <Badge
-                    onClick={() => navigate("/notifications")}
-                >
-                    <i className="ri-notification-line header-action-icon px-3"></i>
-                </Badge>
+              <Badge
+                count={user?.unseenNotifications.length}
+                onClick={() => navigate("/notifications")}
+              >
+                <i className="ri-notification-line header-action-icon px-3"></i>
+              </Badge>
 
-
-                <Link className="anchor mx-2" to="/profile">
-                    {user?.name}
-                </Link>
+              <Link className="anchor mx-2" to="/profile">
+                {user?.name}
+              </Link>
             </div>
-            </div>
+          </div>
 
-            <div className="body">{children}</div>
+          <div className="body">{children}</div>
         </div>
-        </div>
+      </div>
     </div>
-      );
+  );
 }
 
-export default Layout
+export default Layout;
