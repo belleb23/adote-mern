dabf6fd4caabb888b4fad7c65e24a761262db97f
belleb23 <bellebernieri@gmail.com>
2023-10-25 14:26:59 -0300

    update home page bug

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
-
 
 function Home() {
 
   const getData = async () => {
     try {
-      const response = await axios.post("/api/user/get-user-info-by-id", {
+      const response = await axios.post(
+        "/api/user/get-user-info-by-id", {},
+        {
         headers: {
           Authorization: "Bearer " + localStorage.getItem("token"),
         },
@@ -25,7 +26,8 @@ function Home() {
   return (
     <Layout>
       <div>hOME</div>
-  </Layout>
+    </Layout>
+    
   )
 }
 
